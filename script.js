e1fcbc58d9e0a641a39e111960ecd11d50b2f7f1
Dexter Pai <dr.pyiephonepai@gmail.com>
2024-01-29 19:42:08 -0600

    code cleanup

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
+// ----------------------------------------------------------
+// Global Variables
+// ----------------------------------------------------------
 let x = 1;
 let y = 2;
 let str = '';
 console.log(typeof(str));
 
-let btnArray = []
-for (let i = 0; i < 10; i++) {
-    btnArray[i] = document.querySelector(`#btn${i + 1}`);
-    btnArray[i].addEventListener('click', (e) => {
-        str += e.target.textContent;
-        console.log(`clicked btn ${e.target.textContent}`);
-        console.log(str);
-        })
+let display;
+const buttons = [];
+const operations = {
+    add: 'add',
+    subtract: 'subtract',
+    multiply: 'multiply',
+    divide: 'divide',
 }
-console.log(btnArray)
-
-function main(arg) {
-
-}
-
-function operate(a, b, func) {
-    return func(a, b);
-}
-
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
-
-function sum() {
-    let total = null;
-    for (let i = 0; i < arguments.length; i++) {
-        total += arguments[i];
+// ----------------------------------------------------------
+// Constructor
+// ----------------------------------------------------------
+class Button {
+    constructor (name, value) {
+        this.name = name;
+        this.value = value;
+        this.node;
     }
-    return total;
+    initialize() {
+        this.node = document.createElement('button');
+        document.body.appendChild(this.node);
+        this.node.setAttribute('id', `${this.name}`);
+        this.node.setAttribute('class', `btn`);
+        this.node.textContent = `${this.value}`;
+    }
+    addEvent() {
+        this.node.addEventListener('click', () => console.log(`button ${this.value} clicked`));
+    }
+    update() {console.log('updated')}
 }
 
-main();
+// ----------------------------------------------------------
+// Adding DOM Elements
+// ----------------------------------------------------------
+const addingDOM = (() => {
+    // add display
+    (() => {
+        display = document.createElement('textarea');
+        document.body.appendChild(display);
+    })();
+
+    // add buttons
+    (() => {
+        for (let i = 1; i <= 10; i++) {
+            let tmp;
+            if (i == 10) tmp = new Button('btn0', 0);
+            else tmp = new Button(`btn${i}`, i);
+            tmp.initialize();
+            tmp.addEvent();
+            buttons.push(tmp);
+        }
+        console.table(buttons);
+    })();
+})();
+
+
+// console.log(calculate(1,2,operations.add));
+function calculate(num1, num2, operation) {
+    switch (operation) {
+        case(operations.add): return num1 + num2;
+        case(operations.subtract): return num1 - num2;
+        case(operations.multiply): return num1 * num2;
+        case(operations.divide): return num1 / num2;
+    }
+}
+
+
+// ----------------------------------------------------------
+// Summing all numbers
+// ----------------------------------------------------------
+// function sum() {
+//     let total = null;
+//     for (let i = 0; i < arguments.length; i++) {
+//         total += arguments[i];
+//     }
+//     return total;
+// }
